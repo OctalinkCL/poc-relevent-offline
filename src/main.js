b1cdf8bd2ef68d644ccf3b0f8b95778efd0448b8
Juan Maurelia <juan.maurelia@gmail.comgit config --global user.name Juan>
2026-03-06 11:39:02 -0300

    feat: Initialize new Vue.js project with core components, routing, and configurations.

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import './assets/main.css'
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+
+import App from './App.vue'
+import router from './router'
+
+const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+  },
+})
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
