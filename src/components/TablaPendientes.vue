b2a89a08be093e30adc9274021676e4ac70eae84
Juan Maurelia <juan.maurelia@gmail.comgit config --global user.name Juan>
2026-03-06 13:08:02 -0300

    feat: add a search input field to filter the pending items table

--- src/components/TablaPendientes.vue
+++ src/components/TablaPendientes.vue
@@ -1,20 +1,34 @@
 <template>
+  <v-text-field
+    v-model="busqueda"
+    placeholder="Buscar por nombre o RUT..."
+    prepend-inner-icon="mdi-magnify"
+    clearable
+    hide-details
+    density="compact"
+    class="ma-2"
+  />
   <v-data-table
     :headers="headers"
     :items="items"
     :items-per-page="20"
+    :search="busqueda"
     style="cursor: pointer"
     @click:row="(_, { item }) => emit('select', item)"
   />
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
 defineProps({
   items: {
     type: Array,
     required: true,
   },
 })
+
+const busqueda = ref('')
 
 const emit = defineEmits(['select'])
 
