b61b3cfdfebd3af51abde4dcc45983d0c0d60ebc
Juan Maurelia <juan.maurelia@gmail.comgit config --global user.name Juan>
2026-03-14 09:07:41 -0300

    refactor: Replace hardcoded Firebase configuration with environment variables.

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getDatabase } from 'firebase/database'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCE3L93oc7pRYk-Zzd7zxY3Cpy07M_JZ6Q',
-  authDomain: 'expensemanager-ce83d.firebaseapp.com',
-  databaseURL: 'https://expensemanager-ce83d-default-rtdb.firebaseio.com',
-  projectId: 'expensemanager-ce83d',
-  storageBucket: 'expensemanager-ce83d.firebasestorage.app',
-  messagingSenderId: '594470810198',
-  appId: '1:594470810198:web:60c5d995b7aab932dc41f7',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
